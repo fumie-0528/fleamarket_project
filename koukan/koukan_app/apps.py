616683679681eeae1b0c1c9d98c6b59ee663f6ac
Fumie <funismo@gmail.com>
2022-03-23 00:19:01 -0700

    first commit

---
+++ koukan/koukan_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KoukanAppConfig(AppConfig):
+    name = 'koukan_app'
---
+++ koukan/koukan_app/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path     
+from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    path('', views.index),
+    path('jump_toLogin', views.jump_toLogin),
+    path('register', views.register),
+    path('login', views.login),
+    path('logout', views.logout),
+
+    path('products', views.products),
+    path('products/new', views.new),
+    path('products/create', views.create),
+    # path('products/detail/<int:id>', views.detail),
+    
+    path('products/edit/<int:id>', views.edit),
+    # path('products/update/<int:id>', views.update),
+    path('products/delete/<int:id>', views.delete), 
+
+    # path('products/interest/<int:id>', views.interest),
+    # path('products/cancel/<int:id>', views.cancel)
+]
+urlpatterns += staticfiles_urlpatterns()
